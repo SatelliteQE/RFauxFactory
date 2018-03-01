b76b8403b313a5b93e565dec61cf3294d28d8300
ldjebran <ldjebran@gmail.com>
2018-03-01 15:27:09 +0200

    Add travis-ci and coverage support + 2 tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,17 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "rfauxfactory"
+if RUBY_VERSION > "2.5"
+  require 'simplecov'
+  require 'coveralls'
 
+  SimpleCov.formatters = [SimpleCov::Formatter::HTMLFormatter,
+                          Coveralls::SimpleCov::Formatter]
+  SimpleCov.start do
+    minimum_coverage 70
+    maximum_coverage_drop 0.1
+    refuse_coverage_drop
+  end
+end
+
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
+require "rfauxfactory"
 require "minitest/autorun"
